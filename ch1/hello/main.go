6484fd9ec108c16e962f0d404db3b436127b18c2
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-04-21 10:38:36 -0500

    wip

--- ch1/hello/main.go
+++ ch1/hello/main.go
@@ -7,11 +7,13 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-// make a wrapper for winmain
-// make a wrapper for winproc
 func winmain(hInstance windows.Handle, hPrevInstance windows.Handle, lpCmdLine *uint16, nCmdShow int) int {
+	text := windows.StringToUTF16Ptr("Hello, Windows!")
+	caption := windows.StringToUTF16Ptr("HelloMsg")
+	windows.MessageBox(0, text, caption, 0)
 	return 0
 }
+
 func main() {
 	var hInstance windows.Handle
 	err := windows.GetModuleHandleEx(windows.GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, nil, &hInstance)
@@ -27,20 +29,8 @@ func main() {
 	if s.Flags&windows.STARTF_USESHOWWINDOW != 0 {
 		nCmdShow = int(s.ShowWindow)
 	}
-
-	// convert os.Args to lpCmdLine
 	args := strings.Join(os.Args[1:], " ")
 	lpCmdLine := windows.StringToUTF16Ptr(args)
-
+	
 	winmain(hInstance, 0, lpCmdLine, nCmdShow)
-	// MessageBox("Hello, Windows!", "HelloMsg")
-	// println("hInstance:", hInstance)
 }
-
-// func MessageBox(caption, text string) int {
-// 	t, _ := syscall.BytePtrFromString(text)
-// 	c, _ := syscall.BytePtrFromString(caption)
-
-// 	ret, _, _ := MessageBoxA.Call(0, uintptr(unsafe.Pointer(t)), uintptr(unsafe.Pointer(c)), 0)
-// 	return int(ret)
-// }
