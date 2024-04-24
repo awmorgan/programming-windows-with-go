3f89f065d478d255ca0e4835ccac84cd7463e7dd
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-04-23 19:46:26 -0500

    Add hellowin/main.go to display a basic window wip

---
+++ ch3/hellowin/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"syscall"
+
+	"github.com/awmorgan/programming-windows-with-go/internal/sys/windows"
+)
+
+func wndproc(hWnd windows.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	return 0
+}
+
+func winmain(hInstance windows.Handle, hPrevInstance windows.Handle, lpCmdLine *uint16, nCmdShow int) int {
+	var wc windows.WNDCLASSW
+	wc.Style = windows.CS_HREDRAW | windows.CS_VREDRAW
+	wc.LpfnWndProc = syscall.NewCallback(wndproc)
+
+	return 0
+}
+
+func main() {
+	var hInstance windows.Handle
+	err := windows.GetModuleHandleEx(windows.GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, nil, &hInstance)
+	if err != nil {
+		panic(err)
+	}
+	var s windows.StartupInfo
+	err = windows.GetStartupInfo(&s)
+	if err != nil {
+		panic(err)
+	}
+	nCmdShow := windows.SW_SHOWDEFAULT
+	if s.Flags&windows.STARTF_USESHOWWINDOW != 0 {
+		nCmdShow = int(s.ShowWindow)
+	}
+	args := strings.Join(os.Args[1:], " ")
+	lpCmdLine, err := windows.UTF16PtrFromString(args)
+	if err != nil {
+		panic(err)
+	}
+
+	winmain(hInstance, 0, lpCmdLine, nCmdShow)
+}
