27f3cd75aa2d9b018b2550dec8ad9001edad843b
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-06-01 13:37:02 -0500

    environ wip

---
+++ environ/environ.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"x/win32"
+)
+
+const (
+	ID_LIST = 1
+	ID_TEXT = 2
+)
+
+func main() {
+	runtime.LockOSThread() // Windows messages are delivered to the thread that created the window.
+	appName := "Envrion"
+	wc := win32.WNDCLASS{
+		Style:         win32.CS_HREDRAW | win32.CS_VREDRAW,
+		LpfnWndProc:   win32.NewWndProc(wndproc),
+		HInstance:     win32.HInstance(),
+		HIcon:         win32.ApplicationIcon(),
+		HCursor:       win32.ArrowCursor(),
+		HbrBackground: win32.COLOR_WINDOW + 1,
+		LpszClassName: win32.StringToUTF16Ptr(appName),
+	}
+	if _, err := win32.RegisterClass(&wc); err != nil {
+		errMsg := fmt.Sprintf("RegisterClass failed: %v", err)
+		win32.MessageBox(0, errMsg, appName, win32.MB_ICONERROR)
+		return
+	}
+	hwnd, _ := win32.CreateWindow(appName, "Environment List Box",
+		win32.WS_OVERLAPPEDWINDOW,
+		win32.CW_USEDEFAULT, win32.CW_USEDEFAULT,
+		win32.CW_USEDEFAULT, win32.CW_USEDEFAULT,
+		0, 0, win32.HInstance(), 0)
+
+	win32.ShowWindow(hwnd, win32.NCmdShow())
+	win32.UpdateWindow(hwnd)
+	var msg win32.MSG
+	for {
+		ret, err := win32.GetMessage(&msg, 0, 0, 0)
+		if err != nil {
+			errMsg := fmt.Sprintf("GetMessage failed: %v", err)
+			win32.MessageBox(0, errMsg, appName, win32.MB_ICONERROR)
+			os.Exit(1)
+		}
+		if ret == 0 {
+			os.Exit(int(msg.WParam))
+		}
+
+		win32.TranslateMessage(&msg)
+		win32.DispatchMessage(&msg)
+	}
+
+}
+
+func wndproc(hwnd win32.HWND, msg uint32, wParam, lParam uintptr) (result uintptr) {
+	switch msg {
+	case win32.WM_DESTROY:
+		win32.PostQuitMessage(0)
+		return 0
+	}
+	return win32.DefWindowProc(hwnd, msg, wParam, lParam)
+}
