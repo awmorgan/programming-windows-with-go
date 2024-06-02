2e75ae588fa1512326c0b773b81cd059557e415f
Bill Morgan <arthurwilliammorgan@gmail.com>
2024-06-02 12:07:03 -0500

    environ

--- environ/environ.go
+++ environ/environ.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
+	"unsafe"
 	"x/win32"
 )
 
@@ -55,8 +57,59 @@ func main() {
 
 }
 
+func fillListBox(hwndList win32.HWND) {
+	env := os.Environ()
+
+	for _, v := range env {
+		// split the name=value pair
+		nameStr := v[:strings.Index(v, "=")]
+		name := win32.StringToUTF16Ptr(nameStr)
+		win32.SendMessage(hwndList, win32.LB_ADDSTRING, 0, uintptr(unsafe.Pointer(name)))
+	}
+}
+
+var hwndList, hwndText win32.HWND
+
 func wndproc(hwnd win32.HWND, msg uint32, wParam, lParam uintptr) (result uintptr) {
 	switch msg {
+	case win32.WM_CREATE:
+		cxChar := win32.LOWORD(uintptr(win32.GetDialogBaseUnits()))
+		cyChar := win32.HIWORD(uintptr(win32.GetDialogBaseUnits()))
+
+		// create list box and static text windows
+		hwndList, _ = win32.CreateWindow("listbox", "",
+			win32.WS_CHILD|win32.WS_VISIBLE|win32.LBS_STANDARD,
+			cxChar, cyChar*3,
+			cxChar*16+win32.GetSystemMetrics(win32.SM_CXVSCROLL),
+			cyChar*5,
+			hwnd, ID_LIST,
+			win32.HInstance(), 0)
+		hwndText, _ = win32.CreateWindow("static", "",
+			win32.WS_CHILD|win32.WS_VISIBLE|win32.SS_LEFT,
+			cxChar, cyChar,
+			win32.GetSystemMetrics(win32.SM_CXSCREEN), cyChar,
+			hwnd, ID_TEXT,
+			win32.HInstance(), 0)
+		fillListBox(hwndList)
+		return 0
+
+	case win32.WM_SETFOCUS:
+		win32.SetFocus(hwndList)
+		return 0
+
+	case win32.WM_COMMAND:
+		if win32.LOWORD(wParam) == ID_LIST && win32.HIWORD(wParam) == win32.LBN_SELCHANGE {
+			index := win32.SendMessage(hwndList, win32.LB_GETCURSEL, 0, 0)
+			len := win32.SendMessage(hwndList, win32.LB_GETTEXTLEN, index, 0)
+			text := win32.StringToUTF16Ptr(strings.Repeat(" ", int(len)))
+			win32.SendMessage(hwndList, win32.LB_GETTEXT, index, uintptr(unsafe.Pointer(text)))
+
+			// get the environment string
+			envStr := os.Getenv(win32.Utf16PtrToString(text))
+			win32.SetWindowText(hwndText, envStr)
+		}
+		return 0
+
 	case win32.WM_DESTROY:
 		win32.PostQuitMessage(0)
 		return 0
